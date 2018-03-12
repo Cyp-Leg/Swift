0cc9cc9e022c37670a0a4812336ed9f7ca1dd85a
cyp <cyp@cyps-Mac.local>
2018-03-12 19:01:26 +0100

    Nav entre pages ok

---
+++ PkCHU/PkCHU/ActiviteModel.swift
@@ -0,0 +1,17 @@
+//
+//  ActiviteModel.swift
+//  PkCHU
+//
+//  Created by cyp on 12/03/2018.
+//  Copyright © 2018 cyp. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+extension Activite{
+    func ok(){
+    }
+    
+}
